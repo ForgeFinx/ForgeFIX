5d6be09db31dbcead7b4b9f653c76b718524d35a
ForgeFinx <155686123+ForgeFinx@users.noreply.github.com>
2025-03-21 10:24:43 -0400

    forgefix-c-at: use absolute path in build script to link

--- forgefix-c-at/build.rs
+++ forgefix-c-at/build.rs
@@ -1,19 +1,20 @@
-const SOURCE_FILE: &str = "../forgefix-c/fix_client.c"; 
-const OUT_NAME: &str = "fix_client"; 
+const SOURCE_FILE: &str = "../forgefix-c/fix_client.c";
+const OUT_NAME: &str = "fix_client";
 
-const INCLUDES: &[&str] = &[
-    "../forgefix-c",
-];
+const INCLUDES: &[&str] = &["../forgefix-c"];
 
 fn main() {
+    let crate_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+
     println!("cargo:rerun-if-changed={}", SOURCE_FILE);
     cc::Build::new()
         .file(SOURCE_FILE)
         .includes(INCLUDES)
         .compile(OUT_NAME);
 
-    println!("cargo:rustc-link-search-native=../target/debug");
+    println!(
+        "cargo:rustc-link-search=native={}/../target/debug",
+        crate_dir,
+    );
     println!("cargo:rustc-link-lib=dylib=forgefix_c");
 }
-
-
